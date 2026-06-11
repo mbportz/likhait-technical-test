fd200e026846fefd6ac3b00ae7adf84ff45df95c
Marc Bryan Portuguez <marcbryanportuguez@Marcs-MacBook-Pro-2.local>
2026-06-12 07:40:39 +0800

    remove EXPENSE_CATEGORIES

--- frontend/src/components/ExpenseForm.tsx
+++ frontend/src/components/ExpenseForm.tsx
@@ -4,7 +4,6 @@
 
 import React from "react";
 import { ExpenseFormData } from "../types";
-import { EXPENSE_CATEGORIES } from "../constants/categories";
 import { TextField, SelectBox, Button } from "../vibes";
 import { useExpenseForm } from "../hooks/useExpenseForm";
 
